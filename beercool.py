20d3ff572190d3b8bb736116349a8f7f37dde9f0
Blake Rainwater <rainwaterblake@gmail.com>
2014-05-13 15:46:58 -0400

    Latest version

--- beercool.py
+++ beercool.py
@@ -12,11 +12,14 @@ enthfuse = 334 # Joules / gram
 # Temperature of freezing water
 tfreeze = 273.16 # Kelvin
 
+# Temperature of an average freezer
+freezert = 255.372
 # Grams per gallon of water
 gramgallon = 3785.4
-
 # Ice cubes a gallon of water could make
 icegallon = 302.8
+# Grams in an ice cube
+icegram = 12.5
 
 # The temperature of a freezer is typically 0 degrees fahrenheit or 255.372 Kelvin
 
@@ -25,39 +28,6 @@ def f_to_k(tempf):
 
 def k_to_f(tempk):
     return (tempk * 9.0 / 5.0) - 459.67
-
-# Energy released cooling water, mass in grams, temperature in Kelvin
-def energy_of_water_cool(mass, temp_diff):
-    return shwater * mass * temp_diff
-
-# Energy required to bring ice up to melting point or energy required to bring water down to freezing point, mass in grams, temperature in Kelvin
-def energy_of_ice_1(mass, temp_init):
-    if temp_init <= tfreeze:
-        return shice * mass * (tfreeze - temp_init)
-    else:
-        return shwater * mass * (tfreeze - temp_init)
-
-# Energy required to change phase of ice to water, mass in grams
-def energy_of_ice_2(mass):
-    return enthfuse * mass
-
-# Energy required to bring water from freezing point to desired temperature
-def energy_of_ice_3(mass, temp_end):
-    if temp_end >= tfreeze:
-        return shwater * mass * (temp_end - tfreeze)
-    else:
-        return shice * mass * (temp_end - tfreeze)
-
-# Energy required to bring ice (or water) up to desired temperature OR energy released bringing water (or ice) down to desired temperature
-def energy_of_ice_warm(mass, temp_init, temp_end):
-    e1 = energy_of_ice_1(mass, temp_init)
-    e2 = 0.0
-    if temp_init <= tfreeze and temp_end > tfreeze:
-        e2 = energy_of_ice_2(mass)
-    elif temp_init > tfreeze and temp_end <= tfreeze:
-        e2 = 0.0 - energy_of_ice_2(mass)
-    e3 = energy_of_ice_3(mass, temp_end)
-    return e1 + e2 + e3
 
 # Calculate Joules per gram for difference in temperature
 
@@ -76,6 +46,8 @@ def jpg(temp_low, temp_high):
     jpg1 = 0.0
     if temp_low < tfreeze:
         jpg1 = jpg_1(temp_low)
+    else:
+        jpg1 = 0.0 - jpg_3(temp_low)
     jpg2 = 0.0
     if temp_low <= tfreeze and temp_high >= tfreeze:
         jpg2 = jpg_2()
@@ -91,31 +63,31 @@ def jpg(temp_low, temp_high):
 # Want:
 #  Mass of cold
 
+# Temperature in Kelvin, mass in grams
 def masscold(mass_hot, temp_want, temp_hot, temp_cold):
+#    print("mh: " + str(mass_hot))
+#    print("tw: " + str(temp_want))
+#    print("th: " + str(temp_hot))
+#    print("tc: " + str(temp_cold))
     # Joules per gram needed to remove to convert h2o at hot to wanted temperature 
     jpg_htw = jpg(temp_want, temp_hot)
     # Joules per gram required to heat h2o from cold to wanted temperature
     jpg_ctw = jpg(temp_cold, temp_want)
-    
+#    print("jpg_htw: " + str(jpg_htw))
+#    print("jpg_ctw: " + str(jpg_ctw))
     # Mass of cold is Joules removed from hot divided by jpg_ctw, which is ((jpg_htw * mass_hot) / jpg_ctw)
     return (jpg_htw * mass_hot) / jpg_ctw
 
-
-# Temperature is in Kelvin, mass will be returned in whatever unit you put in
-def massofcold(mass_hot, temp_want, temp_hot, temp_cold=255.372):
-    top = shwater * mass_hot * (temp_hot - temp_want)
-    bottom = (temp_want - tfreeze) * shwater
-    if temp_cold <= tfreeze:
-        bottom += enthfuse + (tfreeze - temp_cold) * shice
-    return top / bottom
-
-# Temperature is in fahrenheit, mass is returned in whatever unit is put in
+# Temperature is in fahrenheit, mass is in grams
 def fcoldmass(mass_hot, temp_want, temp_hot, temp_cold=0.0):
-    return massofcold(mass_hot, f_to_k(temp_want), f_to_k(temp_hot), f_to_k(temp_cold))
+    return masscold(mass_hot, f_to_k(temp_want), f_to_k(temp_hot), f_to_k(temp_cold))
 
-# temp in fahrenheit
+# temp in fahrenheit, amount is in gallons, returns the number of grams of ice needed
+def ice_cube_grams(gallons_hot, temp_want, temp_hot, temp_cold=0.0):
+    return fcoldmass(gallons_hot * gramgallon, temp_want, temp_hot, temp_cold)
+
 def num_ice_cubes(gallons_hot, temp_want, temp_hot, temp_cold=0.0):
-    return fcoldmass(gallons_hot, temp_want, temp_hot, temp_cold) * icegallon
+    return ice_cube_grams(gallons_hot, temp_want, temp_hot, temp_cold) / icegram
 
 """def main():
     parser = argparse.ArgumentParser()
@@ -129,6 +101,9 @@ def num_ice_cubes(gallons_hot, temp_want, temp_hot, temp_cold=0.0):
 
 print(masscold(4, tfreeze+0.0001, tfreeze+20, tfreeze))
 
+print(masscold(400, 60 + tfreeze, 61 + tfreeze, tfreeze-10))
+print(masscold(400, 30 + tfreeze, 31 + tfreeze, tfreeze-10))
+
 #print(massofcold(3785, 338, 339))
 #print(k_to_f(338))
 #print(k_to_f(339))
@@ -137,8 +112,9 @@ print(masscold(4, tfreeze+0.0001, tfreeze+20, tfreeze))
 # 1 gallon at 151f lower 1 degree f with ice at freezing
 tice = 0
 tbase = 50
-#while tbase < 210:
-#    print(tbase)
-#    print(num_ice_cubes(1, tbase, tbase + 1, tice))
-#    print(fcoldmass(1, tbase, tbase+1, tice) * gramgallon)
-#    tbase += 10
+while tbase < 210:
+    print(tbase)
+    print(num_ice_cubes(1, tbase, tbase + 1, tice))
+    print(fcoldmass(1, tbase, tbase+1, tice) * gramgallon)
+    print()
+    tbase += 10
